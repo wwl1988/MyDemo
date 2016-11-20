63bc43a0e390ecadac7f10cc4fe185a1470a6c73
wangwl <wwl198800@163.com>
2016-11-20 21:04:31 +0800

    2016-11-20 （2） test commit

--- mywatchertablecustom/src/main/java/com/example/wwl/mywatchertablecustom/MainActivity.java
+++ mywatchertablecustom/src/main/java/com/example/wwl/mywatchertablecustom/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.wwl.mywatchertablecustom;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 主Activity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
