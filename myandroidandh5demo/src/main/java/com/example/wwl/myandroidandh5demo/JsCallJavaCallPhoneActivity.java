a38dedaa2bc3ecbb817a2dba842bc96bcef1c7e9
wangwl <wwl198800@163.com>
2016-12-05 18:19:10 +0800

    2016-12-05 项目： MyDemo-myandroidandh5demo 描述： 1、互调项目实现可以运行，以及拨打电话的实现

--- myandroidandh5demo/src/main/java/com/example/wwl/myandroidandh5demo/JsCallJavaCallPhoneActivity.java
+++ myandroidandh5demo/src/main/java/com/example/wwl/myandroidandh5demo/JsCallJavaCallPhoneActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Handler;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -36,19 +37,23 @@ public class  JsCallJavaCallPhoneActivity extends AppCompatActivity {
 
     }
 
+    private Handler mHandler = new Handler();
+
     class AndroidAndJSInterface2 {
         /**
          * 被Js调用，用于加载数据
          */
         @JavascriptInterface
         public void showcontacts(){
-            Log.d("WWL", "showcontacts是否被调用");
-            String json = "[{\"name\":\"老汪\",\"phone\":\"18600868377\"}]";
-//            mWebView.loadUrl("javascript:show('"+ json +"')");
-//            String json2 = "[{\"name\":\"阿福\", \"phone\":\"18600012345\"}]";
-            String json2 = "[{name:'阿福', phone:'18600012345'}]";
-//            mWebView.loadUrl("javascript:show('" + json2 + "')");
-            mWebView.loadUrl("javascript:showJson(" + "'" + json2 + "'" +")");
+            //必须handler，否则无法正常调用mWebView.loadUrl。
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    String json = "[{\"name\":\"老汪\",\"phone\":\"18600868377\"}]";
+                    mWebView.loadUrl("javascript:showJson(" + "'" + json + "'" +")");
+                }
+            });
+
         }
 
         /**
@@ -58,18 +63,22 @@ public class  JsCallJavaCallPhoneActivity extends AppCompatActivity {
         @JavascriptInterface
         public void call(String phone){
             Toast.makeText(JsCallJavaCallPhoneActivity.this, "phone:"+phone, Toast.LENGTH_LONG).show();
-            /*Intent intent = new Intent();
+            Intent intent = new Intent();
             //检查是否具备拨打电话的权限，如果没有，请求打开
+            Log.d("WWL", "权限判断1："+ ActivityCompat.checkSelfPermission(JsCallJavaCallPhoneActivity.this, Manifest.permission.CALL_PHONE));
+            Log.d("WWL", "权限判断2："+ PackageManager.PERMISSION_GRANTED);
             if(ActivityCompat.checkSelfPermission(JsCallJavaCallPhoneActivity.this, Manifest.permission.CALL_PHONE)
                     != PackageManager.PERMISSION_GRANTED){
+                Log.d("WWL", "权限被禁止了。。。");
                 intent.setAction(Settings.ACTION_MANAGE_WRITE_SETTINGS);
                 intent.setData(Uri.parse("package:" + getPackageName()));
                 startActivityForResult(intent, 555);
             } else {
+                Log.d("WWL", "权限被允许了。。。可以打电话了");
                 intent.setAction(Intent.ACTION_CALL);
                 intent.setData(Uri.parse("tel:" + phone));
                 startActivity(intent);
-            }*/
+            }
         }
 
     }
